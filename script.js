ba2ed71e1fa6e78a23cd83877ec90820b5254fde
Kaedanyu <kaedan.yu@mail.utoronto.ca>
2026-02-08 19:38:41 -0500

    update

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2FlZGFueXUiLCJhIjoiY21rYXJpNHF6MGltODNkcHE3dHM5cmxlZyJ9.nnYd9wh7kN2DJFgtuewiyg'
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FlZGFueXUiLCJhIjoiY21rYXJpNHF6MGltODNkcHE3dHM5cmxlZyJ9.nnYd9wh7kN2DJFgtuewiyg' //mapbox public token
 
 const map = new mapboxgl.Map({
-    container: 'my-map',
-    style: 'mapbox://styles/kaedanyu/cml6bsqrf005q01s6e2sxgikd',
-    center: [-79.385284, 43.654956],
-    zoom: 10,
-    pitch: 70,
+    container: 'my-map', //map container id
+    style: 'mapbox://styles/kaedanyu/cml6bsqrf005q01s6e2sxgikd', // style url
+    center: [-79.385284, 43.654956], //starting position viewing downtown Toronto
+    zoom: 10, //zoomed in
+    pitch: 70, //angled camera view
 });
 
 map.on('load', () => {
 
-    map.addSource('buildings-data', {
+    map.addSource('buildings-data', { //add geoJSON from github repo
         type: 'geojson',
-        data: 'https://raw.githubusercontent.com/Kaedanyu/ExerciseV/main/wk5-data/buildings.geojson' //URL to geojson file via github (update once published!)
+        data: 'https://raw.githubusercontent.com/Kaedanyu/Lab-2/main/Lost_Rivers_20170705__last_edited.geojson' //URL to geojson file via github (update once published!) ***WAIT TO WHAT?*
     });
 
 
     map.addLayer({
-        'id': 'buildings-pnt',
-        'type': 'circle',
+        'id': 'rivers-line',
+        'type': 'line',
         'source': 'buildings-data',
         'paint': {
-            'circle-radius': 20,
-            'circle-color': '#2bff00'
+            'line-color': '#B4D2FF',
+            'line-width': 2
         }
     });
 
-   
+
 });
